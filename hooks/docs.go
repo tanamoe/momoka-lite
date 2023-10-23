5161c93bd380c32577c84dcf4bf4b2d9b4faa806
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-10-23 19:15:44 +0700

    feat: add docs hook

---
+++ hooks/docs.go
@@ -0,0 +1,21 @@
+package hooks
+
+import (
+	"os"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+	"tana.moe/momoka-lite/models"
+)
+
+func registerDocsHandler(
+	app *pocketbase.PocketBase,
+	context *models.AppContext,
+) error {
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+                e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./docs/.vitepress/dist"), false))
+                return nil
+	})
+	return nil
+}
