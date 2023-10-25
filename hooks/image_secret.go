22a35f47181c8919bd5f4c7ff3b213d140d7118c
momo-p <contact@relay.akimitsu.dev>
2023-10-25 09:36:39 +0700

    chore(imagor): refactor the hooks to follow golang standard

--- hooks/image_secret.go
+++ hooks/image_secret.go
@@ -4,6 +4,7 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
+	"fmt"
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
@@ -11,47 +12,27 @@ import (
 	"tana.moe/momoka-lite/models"
 )
 
+const (
+	imageCoversField           = "covers"
+	imageCoverField            = "cover"
+	imageCoversCollectionField = "coversCollection"
+	imageCoversRecordField     = "coversRecord"
+)
+
 func registerAppendImageSecretHook(
 	app *pocketbase.PocketBase,
 	context *models.AppContext,
 ) error {
+	targetCollections := []string{"titles", "books", "publications", "bookDetails"}
 	secret := context.ImagorSecret
 
-	app.OnRecordViewRequest("titles").Add(func(e *core.RecordViewEvent) error {
+	app.OnRecordViewRequest(targetCollections...).Add(func(e *core.RecordViewEvent) error {
 		return appendImageSecret(secret, e.Record)
 	})
 
-	app.OnRecordsListRequest("titles").Add(func(e *core.RecordsListEvent) error {
+	app.OnRecordsListRequest(targetCollections...).Add(func(e *core.RecordsListEvent) error {
 		for _, record := range e.Records {
 			if err := appendImageSecret(secret, record); err != nil {
-				return err
-			}
-		}
-
-		return nil
-	})
-
-	app.OnRecordViewRequest("books", "publications").Add(func(e *core.RecordViewEvent) error {
-		return appendImageSliceSecret(secret, e.Record)
-	})
-
-	app.OnRecordsListRequest("books", "publications").Add(func(e *core.RecordsListEvent) error {
-		for _, record := range e.Records {
-			if err := appendImageSliceSecret(secret, record); err != nil {
-				return err
-			}
-		}
-
-		return nil
-	})
-
-	app.OnRecordViewRequest("bookDetails").Add(func(e *core.RecordViewEvent) error {
-		return appendImageSliceSecret(secret, e.Record)
-	})
-
-	app.OnRecordsListRequest("bookDetails").Add(func(e *core.RecordsListEvent) error {
-		for _, record := range e.Records {
-			if err := appendImageSliceSecret(secret, record); err != nil {
 				return err
 			}
 		}
@@ -63,46 +44,60 @@ func registerAppendImageSecretHook(
 }
 
 func appendImageSecret(secret string, record *m.Record) error {
-	cover := record.GetString("cover")
+	if _, exist := record.SchemaData()[imageCoversField]; exist {
+		return appendImageSliceSecret(secret, record)
+	}
+
+	cover := record.GetString(imageCoverField)
 
 	if cover == "" {
 		return nil
 	}
 
-	path := record.Collection().Id + "/" + record.Id + "/" + cover
+	path := fmt.Sprintf("%s/%s/%s", record.Collection().Id, record.Id, cover)
 
-	record.Set("metadata", map[string]interface{}{
-		"images": getImageSizes(secret, path),
-	},
+	record.Set(
+		"metadata",
+		map[string]interface{}{
+			"images": getImageSizes(secret, path),
+		},
 	)
 
 	return nil
 }
 
 func appendImageSliceSecret(secret string, record *m.Record) error {
-	covers := record.GetStringSlice("covers")
+	covers := record.GetStringSlice(imageCoversField)
 
 	var images []map[string]string
 
 	for _, cover := range covers {
-		path := record.Collection().Id + "/" + record.Id + "/" + cover
+		path := fmt.Sprintf("%s/%s/%s", record.Collection().Id, record.Id, cover)
 		images = append(images, getImageSizes(secret, path))
 	}
 
-	record.Set("metadata", map[string]interface{}{
-		"images": images,
-	})
+	record.Set(
+		"metadata",
+		map[string]interface{}{
+			"images": images,
+		},
+	)
 
 	return nil
 }
 
 func appendImageSizeMetadata(secret string, record *m.Record) error {
-	covers := record.GetStringSlice("covers")
+	covers := record.GetStringSlice(imageCoversField)
 
 	var images []map[string]string
 
 	for _, cover := range covers {
-		path := record.GetString("coversCollection") + "/" + record.GetString("coversRecord") + "/" + cover
+		path := fmt.Sprintf(
+			"%s/%s/%s",
+			record.GetString(imageCoversCollectionField),
+			record.GetString(imageCoversRecordField),
+			cover,
+		)
 		images = append(images, getImageSizes(secret, path))
 	}
 
@@ -115,18 +110,18 @@ func appendImageSizeMetadata(secret string, record *m.Record) error {
 
 func getImageSizes(secret string, path string) map[string]string {
 	return map[string]string{
-		"160w":  signImageHmac(secret, "120x0/filters:quality(90)/"+path),
-		"320w":  signImageHmac(secret, "320x0/filters:quality(90)/"+path),
-		"480w":  signImageHmac(secret, "480x0/filters:quality(90)/"+path),
-		"640w":  signImageHmac(secret, "640x0/filters:quality(90)/"+path),
-		"1280w": signImageHmac(secret, "1280x0/filters:quality(90)/"+path),
-		"1920w": signImageHmac(secret, "1920x0/filters:quality(90)/"+path),
+		"160w":  signImageUrl(secret, fmt.Sprintf("120x0/filters:quality(90)/%s", path)),
+		"320w":  signImageUrl(secret, fmt.Sprintf("320x0/filters:quality(90)/%s", path)),
+		"480w":  signImageUrl(secret, fmt.Sprintf("480x0/filters:quality(90)/%s", path)),
+		"640w":  signImageUrl(secret, fmt.Sprintf("640x0/filters:quality(90)/%s", path)),
+		"1280w": signImageUrl(secret, fmt.Sprintf("1280x0/filters:quality(90)/%s", path)),
+		"1920w": signImageUrl(secret, fmt.Sprintf("1920x0/filters:quality(90)/%s", path)),
 	}
 }
 
-func signImageHmac(secret string, path string) string {
+func signImageUrl(secret string, path string) string {
 	mac := hmac.New(sha256.New, []byte(secret))
 	mac.Write([]byte(path))
-
-	return base64.URLEncoding.EncodeToString(mac.Sum(nil))[:40] + "/" + path
+	sign := base64.URLEncoding.EncodeToString(mac.Sum(nil))[:40]
+	return fmt.Sprintf("%s/%s", sign, path)
 }
