952dae7beb12b1f0b6d75fd2a4ec803178c23889
momo-p <contact@relay.akimitsu.dev>
2023-11-18 03:50:09 +0700

    feat(models): implement users model

---
+++ models/user.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"github.com/pocketbase/pocketbase/models"
+)
+
+var _ models.Model = (*User)(nil)
+
+type User struct {
+	models.BaseModel
+
+	Username    string `db:"username" json:"username"`
+	Email       string `db:"email" json:"email"`
+	DisplayName string `db:"displayName" json:"displayName"`
+	Bio         string `db:"bio" json:"bio"`
+	Avatar      string `db:"avatar" json:"avatar"`
+	Banner      string `db:"banner" json:"banner"`
+}
+
+func (m *User) TableName() string {
+	return "users"
+}
