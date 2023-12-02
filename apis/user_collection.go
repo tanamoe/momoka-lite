2dc2571ad1480ba31314ac85defed92a88d098bd
momo-p <contact@relay.akimitsu.dev>
2023-12-02 10:41:38 +0700

    feat: implement fetch user default collection route

--- apis/user_collection.go
+++ apis/user_collection.go
@@ -30,6 +30,9 @@ func onRequestUserCollectionById(
 	admin, _ := c.Get(apis.ContextAdminKey).(*pmodels.Admin)
 	record, _ := c.Get(apis.ContextAuthRecordKey).(*pmodels.Record)
 	collectionId := c.PathParam("collectionId")
+	if collectionId == "default" {
+		return onRequestUserDefaultCollection(app, e, c, expand)
+	}
 	collection, err := models.FindCollectionById(app.Dao(), collectionId)
 	if err != nil {
 		return nil, err
@@ -47,5 +50,31 @@ func onRequestUserCollectionById(
 	if !canBeAccessed {
 		return nil, notFoundError
 	}
-	return nil, notFoundError
+	if err := collection.Expand(app.Dao(), expand); err != nil {
+		return nil, err
+	}
+	return collection, nil
+}
+
+func onRequestUserDefaultCollection(
+	app *pocketbase.PocketBase,
+	e *core.ServeEvent,
+	c echo.Context,
+	expand models.ExpandMap,
+) (item *models.Collection, err error) {
+	record, _ := c.Get(apis.ContextAuthRecordKey).(*pmodels.Record)
+	if record == nil {
+		return nil, unauthorizedError
+	}
+	collection, err := models.FindUserDefaultCollection(app.Dao(), record.Id)
+	if err != nil {
+		return nil, err
+	}
+	if collection == nil {
+		return nil, notFoundError
+	}
+	if err := collection.Expand(app.Dao(), expand); err != nil {
+		return nil, err
+	}
+	return collection, nil
 }
