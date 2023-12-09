1dd3e5384b474a4759e0c12cdf3c3e0f0418b6fd
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-12-09 22:07:38 +0700

    feat(book): implement publication expand

--- models/book.go
+++ models/book.go
@@ -14,13 +14,15 @@ var _ models.Model = (*Book)(nil)
 type Book struct {
 	models.BaseModel
 
-	PublicationID    string                  `db:"publication" json:"publication"`
+	PublicationID    string                  `db:"publication" json:"publicationId"`
+	Publication      *Publication            `db:"-" json:"publication,omitempty"`
 	Edition          string                  `db:"edition" json:"edition"`
 	PublishDate      types.DateTime          `db:"publishDate" json:"publishDate"`
 	Covers           types.JsonArray[string] `db:"covers" json:"covers"`
 	Price            int                     `db:"price" json:"price"`
 	Note             string                  `db:"note" json:"note"`
 	Metadata         types.JsonMap           `db:"metadata" json:"metadata"`
+	ParentID         string                  `db:"parentId" json:"parentId"`
 	ParentCollection string                  `db:"parentCollection" json:"parentCollection"`
 }
 
@@ -48,5 +50,17 @@ func FindBookById(dao *daos.Dao, id string) (*Book, error) {
 }
 
 func (m *Book) Expand(dao *daos.Dao, e ExpandMap) error {
+	if e == nil {
+		return nil
+	}
+
+	if _, exist := e["publication"]; exist {
+		publication, err := FindPublicationById(dao, m.PublicationID)
+		if err != nil {
+			return err
+		}
+		m.Publication = publication
+	}
+
 	return nil
 }
