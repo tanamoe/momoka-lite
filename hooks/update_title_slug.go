cc46664899ca081778d34e5478106a03a75c7065
momo-p <contact@relay.akimitsu.dev>
2023-12-13 00:08:29 +0700

    feat(titles): automate assign slug when record upsert

---
+++ hooks/update_title_slug.go
@@ -0,0 +1,54 @@
+package hooks
+
+import (
+	"github.com/gosimple/slug"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+	"tana.moe/momoka-lite/models"
+	"tana.moe/momoka-lite/services"
+)
+
+func registerUpdateTitleSlugHook(
+	app *pocketbase.PocketBase,
+	context *models.AppContext,
+) error {
+	app.
+		OnModelAfterCreate((&models.Title{}).TableName()).
+		Add(func(e *core.ModelEvent) error {
+			return updateTitleSlug(app, context, e)
+		})
+
+	app.
+		OnModelAfterUpdate((&models.Title{}).TableName()).
+		Add(func(e *core.ModelEvent) error {
+			return updateTitleSlug(app, context, e)
+		})
+	return nil
+}
+
+func updateTitleSlug(
+	app *pocketbase.PocketBase,
+	context *models.AppContext,
+	e *core.ModelEvent,
+) error {
+	titleId := e.Model.GetId()
+	title, err := models.FindTitleById(app.Dao(), titleId)
+	if err != nil {
+		return err
+	}
+	if title.SlugGroup == "" {
+		title.SlugGroup = slug.Make(title.Name)
+		title.Slug = title.SlugGroup
+		if err := app.Dao().Save(title); err != nil {
+			return err
+		}
+	}
+
+	if err := services.UpdateTitleSlug(
+		app.Dao().WithoutHooks(),
+		title.SlugGroup,
+	); err != nil {
+		return err
+	}
+	return nil
+}
