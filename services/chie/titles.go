1c4e69826ffe8510a7ce440ce17fa606a17fa2d0
momo-p <contact@relay.akimitsu.dev>
2024-01-14 00:28:20 +0700

    feat: implement search signal support

--- services/chie/titles.go
+++ services/chie/titles.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/blevesearch/bleve"
+	"github.com/blevesearch/bleve/search/query"
 	"github.com/gosimple/slug"
 	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
@@ -23,6 +24,7 @@ type TitleSearchRequest struct {
 	Staffs      QueryGroup[string] `json:"staffs"`
 	Limit       int                `json:"limit"`
 	Offset      int                `json:"offset"`
+	SortBy      string             `json:"sort"`
 }
 
 type TitleSearchResponse struct {
@@ -39,6 +41,8 @@ type titleIndex struct {
 	Demographic string                  `json:"demographic" db:"demographic"`
 	Genres      types.JsonArray[string] `json:"genres" db:"genres"`
 	Staffs      []string                `json:"staffs" db:"staffs"`
+	Created     string                  `json:"created" db:"created"`
+	Updated     string                  `json:"updated" db:"updated"`
 }
 
 type titleSearchSignal struct {
@@ -102,8 +106,74 @@ func startTitleSearchService(app *pocketbase.PocketBase, context *models.AppCont
 	return nil
 }
 
+func SearchForTitles(query TitleSearchRequest) TitleSearchResponse {
+	res := make(chan TitleSearchResponse)
+	titleSearchChannel <- titleSearchSignal{
+		Query:  query,
+		Result: res,
+	}
+	return <-res
+}
+
+func UpdateTitleIndex(dao *daos.Dao, title *models.Title) error {
+	err := make(chan error)
+	titleUpdateChannel <- titleUpdateSignal{
+		Dao:   dao,
+		Title: title,
+		Err:   err,
+	}
+	return <-err
+}
+
 func searchForTitles(req TitleSearchRequest) (int, []string, error) {
-	return 0, nil, nil
+	var queries []query.Query
+	if req.Name != "" {
+		q := query.NewMatchQuery(normalizeTitleName(req.Name))
+		q.SetField("tags")
+		queries = append(queries, q)
+	}
+	if len(req.Format.Values) > 0 {
+		queries = append(queries, groupTermQuery[string](req.Format, "format"))
+	}
+	if len(req.Demographic.Values) > 0 {
+		queries = append(queries, groupTermQuery[string](req.Demographic, "demographic"))
+	}
+	if len(req.Genres.Values) > 0 {
+		queries = append(queries, groupTermQuery[string](req.Genres, "genres"))
+	}
+	if len(req.Staffs.Values) > 0 {
+		staffsQuery := QueryGroup[string]{
+			Kind: req.Staffs.Kind,
+		}
+		for _, staff := range req.Staffs.Values {
+			staffsQuery.Values = append(staffsQuery.Values, normalizeTitleStaffName(staff))
+		}
+		fmt.Printf("%s\n", staffsQuery)
+		queries = append(queries, groupMatchQuery[string](staffsQuery, "staffs"))
+	}
+	searchQuery := query.NewConjunctionQuery(queries)
+	searchReq := bleve.NewSearchRequest(searchQuery)
+	if req.Limit <= 0 {
+		req.Limit = 50
+	}
+	if req.Limit > 200 {
+		req.Limit = 200
+	}
+	searchReq.Size = req.Limit
+	searchReq.From = req.Offset
+	switch req.SortBy {
+	case "name", "-name", "created", "-created", "updated", "-updated":
+		searchReq.SortBy([]string{req.SortBy})
+	}
+	result, err := titleIndexMapping.Search(searchReq)
+	if err != nil {
+		return 0, nil, err
+	}
+	var hits []string
+	for _, hit := range result.Hits {
+		hits = append(hits, hit.ID)
+	}
+	return int(result.Total), hits, nil
 }
 
 func updateTitleIndex(dao *daos.Dao, title *models.Title) error {
