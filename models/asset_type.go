4655181516f49fd1af119947d75c65bd8b542b3b
Nguyen Do <khoanguyen.do@outlook.com>
2024-07-08 19:20:23 +0700

    feat(asset): add models

---
+++ models/asset_type.go
@@ -0,0 +1,44 @@
+package models
+
+import (
+	"database/sql"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	"github.com/pocketbase/pocketbase/models"
+)
+
+var _ models.Model = (*AssetType)(nil)
+
+type AssetType struct {
+	models.BaseModel
+
+	Name string `db:"name" json:"name"`
+}
+
+func (m *AssetType) TableName() string {
+	return "assetType"
+}
+
+func AssetTypeQuery(dao *daos.Dao) *dbx.SelectQuery {
+	return dao.ModelQuery(&AssetType{})
+}
+
+func FindAssetTypeById(dao *daos.Dao, id string) (*AssetType, error) {
+	assetType := &AssetType{}
+	err := AssetTypeQuery(dao).
+		AndWhere(dbx.HashExp{"id": id}).
+		Limit(1).
+		One(assetType)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return assetType, nil
+}
+
+func (m *AssetType) Expand(dao *daos.Dao, e ExpandMap) error {
+	return nil
+}
