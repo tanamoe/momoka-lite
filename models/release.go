e6a99a3af38bc0a37a2769a3b11a6bb79d89f883
momo-p <momo-p@chong-arisu.id.vn>
2024-07-16 00:08:27 +0700

    feat(release): sync models fields

--- models/release.go
+++ models/release.go
@@ -25,12 +25,16 @@ var _ models.Model = (*Release)(nil)
 type Release struct {
 	models.BaseModel
 
-	TitleId     string        `db:"title" json:"titleId"`
-	Title       *Title        `db:"-" json:"title,omitempty"`
-	Name        string        `db:"name" json:"name"`
-	PublisherId string        `db:"publisher" json:"publisherId"`
-	Publisher   *Publisher    `db:"-" json:"publisher,omitempty"`
-	Status      ReleaseStatus `db:"status" json:"status"`
+	TitleId        string        `db:"title" json:"titleId"`
+	Title          *Title        `db:"-" json:"title,omitempty"`
+	Name           string        `db:"name" json:"name"`
+	Type           string        `db:"type" json:"type"`
+	Disambiguation string        `db:"disambiguation" json:"disambiguation"`
+	PublisherId    string        `db:"publisher" json:"publisherId"`
+	Publisher      *Publisher    `db:"-" json:"publisher,omitempty"`
+	PartnerId      string        `db:"partner" json:"partnerId"`
+	Partner        *Publisher    `db:"-" json:"partner,omitempty"`
+	Status         ReleaseStatus `db:"status" json:"status"`
 }
 
 func (m *Release) TableName() string {
@@ -84,6 +88,19 @@ func (m *Release) Expand(dao *daos.Dao, e ExpandMap) error {
 				return err
 			}
 			m.Publisher = publisher
+		}
+	}
+
+	if _, exist := e["partner"]; exist {
+		partner, err := FindPublisherById(dao, m.PartnerId)
+		if err != nil {
+			return err
+		}
+		if partner != nil {
+			if err := partner.Expand(dao, e["partner"]); err != nil {
+				return err
+			}
+			m.Partner = partner
 		}
 	}
 
